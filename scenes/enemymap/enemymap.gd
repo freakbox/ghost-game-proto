2b43ad8f134f91aa79e5d500aee1589e293a326e
freakbox <freakbox.games@gmail.com>
2025-09-17 19:17:30 -0300

    player map

---
+++ scenes/enemymap/enemymap.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
+@export var speed: float = 80
+
+var _chasing_player: bool = false
+var _player: CharacterBody2D
+
+@onready var battle_trigger_area_2d: Area2D = $BattleTriggerArea2D
+
+func _ready() -> void:
+	battle_trigger_area_2d.body_entered.connect(call_battle_scene)
+
+
+func _physics_process(delta: float) -> void:
+	if _chasing_player:
+		# Direction from this character to the target
+		var direction := (_player.global_position - global_position).normalized()
+	
+		# Move towards the target
+		velocity = direction * speed
+		move_and_slide()
+		
+	else:
+		if ray_cast_2d.is_colliding():
+			print('chase player')
+			_chasing_player = true
+			_player = ray_cast_2d.get_collider()
+	
+
+
+func call_battle_scene(body: CharacterBody2D) -> void:
+	get_tree().call_deferred("change_scene_to_file", "res://scenes/battle/battle.tscn")
+	
---
+++ scenes/playermap/playermap.gd
@@ -0,0 +1,6 @@
+extends CharacterBody2D
+
+func _physics_process(delta: float) -> void:
+	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * 100
+	move_and_slide()
