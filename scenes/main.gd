5822756a0e90e4100e2bddfa54341b0fc8349638
freakbox <freakbox.games@gmail.com>
2026-03-01 09:44:10 -0300

    changing to ghost game proto

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,11 +1,1 @@
 extends Node2D
-
-const BATTLE = preload("res://scenes/battle/battle.tscn")
-
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_packed(BATTLE)
-
-
-func _on_button_2_pressed() -> void:
-	BattleLoader.enemy_speed_relative = 1.6
-	get_tree().change_scene_to_packed(BATTLE)
--- scenes/map/map.gd
+++ scenes/map/map.gd
@@ -5,15 +5,4 @@ extends Node2D
 @onready var whoosh_player: AudioStreamPlayer = $WhooshPlayer
 
 func _ready() -> void:
-	enemymap.player_reached.connect(call_battle_scene)
-
-
-func call_battle_scene() -> void:
-	transition.circle_out(Callable(self, "_go_battle_scene"))
-	whoosh_player.play()
-
-
-func _go_battle_scene():
-	#get_tree().change_scene_to_file("res://scenes/next_scene.tscn")
-	get_tree().call_deferred("change_scene_to_file", "res://scenes/battle/battle.tscn")
-	#transition.fade_in()
+	pass
